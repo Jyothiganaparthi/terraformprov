57cd389b901f4c308638147f7d0d2925849f2b39
Jyothiganaparthi <124667728+Jyothiganaparthi@users.noreply.github.com>
2023-05-21 12:08:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "publicinstance" {
   ami           =  "ami-024e6efaf93d85776"
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
   key_name = "Key98"
   subnet_id     = aws_subnet.publicsubnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
